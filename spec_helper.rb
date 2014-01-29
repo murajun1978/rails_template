b449cfc75fcbb63add6aa6434ddf675ca73478d7
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2014-01-30 00:15:09 +0900

    add file

---
+++ spec_helper.rb
@@ -0,0 +1,34 @@
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+require 'capybara'
+require 'capybara/rspec'
+
+require 'capybara/poltergeist'
+Capybara.javascript_driver = :poltergeist
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Dir.glob("spec/steps/**/*steps.rb") { |f| load f, true }
+
+ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+
+RSpec.configure do |config|
+  config.before :suite do
+    DatabaseRewinder.clean_all
+  end
+
+  config.after :each do
+    DatabaseRewinder.clean
+  end
+
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  config.use_transactional_fixtures = true
+
+  config.infer_base_class_for_anonymous_controllers = false
+
+  config.order = "random"
+
+  config.include FactoryGirl::Syntax::Methods
+end
